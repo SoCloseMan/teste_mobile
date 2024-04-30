de00df29bf74d62614ee185dff450015254d35ec
Gabriel Prado Dos Santos <gps.gabrielprado@gmail.com>
2024-04-30 16:15:08 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teste_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/br/com/gabrielprado/teste_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.gabrielprado.teste_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
